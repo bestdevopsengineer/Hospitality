80f640b77cdbcc3fc1912fb572d237baee4af9c2
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-27 14:15:19 -0500

    dynamodb

--- terraform/components/s3/backend.tf
+++ terraform/components/s3/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "hospitality-terraform-state-502845302465"
     key    = "dev/s3/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table = "hospitality-terraform-locks"
   }
 }
