33eb23c100c5913af5f9e9b71110a1c02936634d
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-27 10:43:23 -0500

    done

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_s3_bucket" "data_bucket" {
-  bucket = "luxury-data-platform-demo-12345"
+  bucket = "luxury-data-platform-${var.environment}-12345"
 
   tags = {
     Name        = "Luxury Data Platform"
-    Environment = "dev"
+    Environment = var.environment
   }
 }
