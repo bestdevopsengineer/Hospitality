78a009092b02eab685e2c50cda487b5132973028
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-27 13:50:13 -0500

    checkov

--- terraform/modules/s3-bucket/main.tf
+++ terraform/modules/s3-bucket/main.tf
@@ -8,6 +8,7 @@ resource "aws_s3_bucket" "this" {
   }
 }
 
+# Ensure that S3 bucket has a Public Access block
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
@@ -15,4 +16,12 @@ resource "aws_s3_bucket_public_access_block" "this" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+# Ensure all data stored in the S3 bucket have versioning enabled
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
