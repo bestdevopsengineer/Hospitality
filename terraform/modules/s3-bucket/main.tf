4b48fbc02b1ceccea4db745530e70a56ded797a6
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-27 13:58:34 -0500

    lifecycle

--- terraform/modules/s3-bucket/main.tf
+++ terraform/modules/s3-bucket/main.tf
@@ -55,5 +55,9 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
     expiration {
       days = 365
     }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
   }
 }
