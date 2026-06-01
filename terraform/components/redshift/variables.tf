ec453a6909a0b237085e8f8ed354ddd42636cfd1
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-06-01 10:09:10 -0500

    add

--- terraform/components/redshift/variables.tf
+++ terraform/components/redshift/variables.tf
@@ -6,3 +6,8 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "master_password" {
+  type      = string
+  sensitive = true
+}
--- terraform/modules/redshift/main.tf
+++ terraform/modules/redshift/main.tf
@@ -1,10 +1,26 @@
 resource "aws_redshift_subnet_group" "this" {
   name       = "${var.environment}-redshift-subnet-group"
   subnet_ids = var.subnet_ids
+  
 
   tags = {
     Name        = "${var.environment}-redshift-subnet-group"
     Environment = var.environment
     Project     = var.project_name
   }
+}
+
+resource "aws_redshift_cluster" "this" {
+  cluster_identifier = "${var.environment}-redshift"
+
+  database_name   = "hospitality"
+  master_username = "adminuser"
+  master_password = var.master_password
+
+  node_type    = "dc2.large"
+  cluster_type = "single-node"
+
+  cluster_subnet_group_name = aws_redshift_subnet_group.this.name
+
+  skip_final_snapshot = true
 }
