096ac48b5850e659897dfadb1c7a9830e1a4bb5f
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-06-01 10:20:21 -0500

    format redshift module

--- terraform/modules/redshift/main.tf
+++ terraform/modules/redshift/main.tf
@@ -1,7 +1,7 @@
 resource "aws_redshift_subnet_group" "this" {
   name       = "${var.environment}-redshift-subnet-group"
   subnet_ids = var.subnet_ids
-  
+
 
   tags = {
     Name        = "${var.environment}-redshift-subnet-group"
