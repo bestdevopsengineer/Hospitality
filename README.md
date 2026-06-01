a752743a55b3afdd9955f39d6e1177280fc7ffb2
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-06-01 13:01:46 -0500

    jjj

--- README.md
+++ README.md
@@ -60,3 +60,10 @@ Table name: hospitality-terraform-locks
 Partition key: LockID
 Type: String
 # 
+
+
+choco --version
+2.5.0
+choco install postgresql --yes
+
+psql --version
--- terraform/modules/redshift/main.tf
+++ terraform/modules/redshift/main.tf
@@ -10,21 +10,48 @@ resource "aws_redshift_subnet_group" "this" {
   }
 }
 
-resource "aws_redshift_cluster" "this" {
-  cluster_identifier = "${var.environment}-redshift"
+# resource "aws_redshift_cluster" "this" {
+#   cluster_identifier = "${var.environment}-redshift"
 
-  database_name   = "hospitality"
-  master_username = "adminuser"
-  master_password = var.master_password
+#   database_name   = "hospitality"
+#   master_username = "adminuser"
+#   master_password = var.master_password
 
-  node_type    = "ra3.xlplus"
-  cluster_type = "single-node"
+#   node_type    = "ra3.xlplus"
+#   cluster_type = "single-node"
 
-  cluster_subnet_group_name = aws_redshift_subnet_group.this.name
+#   cluster_subnet_group_name = aws_redshift_subnet_group.this.name
 
-  publicly_accessible  = false
-  encrypted            = true
-  enhanced_vpc_routing = true
+#   publicly_accessible  = false
+#   encrypted            = true
+#   enhanced_vpc_routing = true
 
-  skip_final_snapshot = true
+#   skip_final_snapshot = true
+# }
+
+resource "aws_redshiftserverless_namespace" "this" {
+  namespace_name = coalesce(var.namespace_name, "${var.environment}-hospitality-namespace")
+
+  db_name             = "hospitality"
+  admin_username      = "adminuser"
+  admin_user_password = var.master_password
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
+resource "aws_redshiftserverless_workgroup" "this" {
+  workgroup_name = coalesce(var.workgroup_name, "${var.environment}-hospitality-workgroup")
+
+  namespace_name = aws_redshiftserverless_namespace.this.namespace_name
+  subnet_ids     = var.subnet_ids
+
+  publicly_accessible = false
+
+  tags = {
+    Environment = var.environment
+    Project     = var.project_name
+  }
 }
